03526ffe48f0f6c01cb95ed5b4a0cb0b5bb3fbfb
Alexander Kentros <crAlexcomp@gmail.com>
2021-02-09 16:50:15 +0200

    Update GoldenIndicator.js

--- GoldenIndicator.js
+++ GoldenIndicator.js
@@ -10,7 +10,7 @@ MA1Type = input(title="MA1 Type", defval="SMA", options=["RMA", "SMA", "EMA", "W
 MA1Source = input(title="MA1 Source", type=input.source, defval=close)
 MA1Visible = input(title="MA1 Visible", type=input.bool, defval=true) // Will automatically hide crossovers containing this MA
 
-MA2Period = input(21, title="MA2 Period")
+MA2Period = input(14, title="MA2 Period")
 MA2Type = input(title="MA2 Type", defval="SMA", options=["RMA", "SMA", "EMA", "WMA", "HMA", "DEMA", "TEMA"])
 MA2Source = input(title="MA2 Source", type=input.source, defval=close)
 MA2Visible = input(title="MA2 Visible", type=input.bool, defval=true) // Will automatically hide crossovers containing this MA
@@ -21,95 +21,58 @@ MA3Source = input(title="MA3 Source", type=input.source, defval=close)
 MA3Visible = input(title="MA3 Visible", type=input.bool, defval=false) // Will automatically hide crossovers containing this MA
 ShowCrosses = input(title="Show Labels", type=input.bool, defval=true)
 
-// MA# is a variable used to store the actual moving average value.
-// if statements - https://www.tradingview.com/pine-script-reference/#op_if
-// MA functions - https://www.tradingview.com/pine-script-reference/ (must search for appropriate MA)
-MA1 = if MA1Type == "SMA"
-    sma(MA1Source, MA1Period)
-else
-    if MA1Type == "EMA"
-        ema(MA1Source, MA1Period)
-    else
-        if MA1Type == "WMA"
-            wma(MA1Source, MA1Period)
-        else
-            if MA1Type == "RMA"
-                rma(MA1Source, MA1Period)
-            else
-                if MA1Type == "HMA"
-                    wma(2*wma(MA1Source, MA1Period/2)-wma(MA1Source, MA1Period), round(sqrt(MA1Period)))
-                else
-                    if MA1Type == "DEMA"
-                        e = ema(MA1Source, MA1Period)
-                        2 * e - ema(e, MA1Period)
-                    else
-                        if MA1Type == "TEMA"
-                            e = ema(MA1Source, MA1Period)
-                            3 * (e - ema(e, MA1Period)) + ema(ema(e, MA1Period), MA1Period)
 
-                    
-MA2 = if MA2Type == "SMA"
-    sma(MA2Source, MA2Period)
-else
-    if MA2Type == "EMA"
-        ema(MA2Source, MA2Period)
+getMA(type,source,period) =>
+    if type == "SMA"
+        sma(source, period)
     else
-        if MA2Type == "WMA"
-            wma(MA2Source, MA2Period)
+        if type == "EMA"
+            ema(source, period)
         else
-            if MA2Type == "RMA"
-                rma(MA2Source, MA2Period)
+            if type == "WMA"
+                wma(source, period)
             else
-                if MA2Type == "HMA"
-                    wma(2*wma(MA2Source, MA2Period/2)-wma(MA2Source, MA2Period), round(sqrt(MA2Period)))
+                if type == "RMA"
+                    rma(source, period)
                 else
-                    if MA2Type == "DEMA"
-                        e = ema(MA2Source, MA2Period)
-                        2 * e - ema(e, MA2Period)
+                    if type == "HMA"
+                        wma(2*wma(source, period/2)-wma(source, period), round(sqrt(period)))
                     else
-                        if MA2Type == "TEMA"
-                            e = ema(MA2Source, MA2Period)
-                            3 * (e - ema(e, MA2Period)) + ema(ema(e, MA2Period), MA2Period)
-                    
-MA3 = if MA3Type == "SMA"
-    sma(MA3Source, MA3Period)
-else
-    if MA3Type == "EMA"
-        ema(MA3Source, MA3Period)
-    else
-        if MA3Type == "WMA"
-            wma(MA3Source, MA3Period)
-        else
-            if MA3Type == "RMA"
-                rma(MA3Source, MA3Period)
-            else
-                if MA3Type == "HMA"
-                    wma(2*wma(MA3Source, MA3Period/2)-wma(MA3Source, MA3Period), round(sqrt(MA3Period)))
-                else
-                    if MA3Type == "DEMA"
-                        e = ema(MA3Source, MA3Period)
-                        2 * e - ema(e, MA3Period)
-                    else
-                        if MA3Type == "TEMA"
-                            e = ema(MA3Source, MA3Period)
-                            3 * (e - ema(e, MA3Period)) + ema(ema(e, MA3Period), MA3Period)
+                        if type == "DEMA"
+                            e = ema(source, period)
+                            2 * e - ema(e, period)
+                        else
+                            if type == "TEMA"
+                                e = ema(source, period)
+                                3 * (e - ema(e, period)) + ema(ema(e, period), period)
 
-transparency = 95
-breakLines= "\n\n\n\n\n"
+MA1 = getMA(MA1Type,MA1Source,MA1Period)
+MA2 = getMA(MA2Type,MA2Source,MA2Period)
+MA3 = getMA(MA3Type,MA3Source,MA3Period)
+
+
+transparency = 100
+breakLines= "\n\n"
+
+// truncate() truncates a given number
+// to a certain number of decimals
+truncate(number, decimals) =>
+    factor = pow(10, decimals)
+    int(number * factor) / factor
 
 // Plotting crossover/unders for all combinations of crosses
 // https://www.tradingview.com/pine-script-reference/v4/#fun_label%7Bdot%7Dnew
-if ShowCrosses and MA1Visible and MA2Visible and crossunder(MA1, MA2)
-    lun1 = label.new(bar_index, na, "Short"+breakLines, 
+if ShowCrosses and MA1Visible and MA2Visible and crossunder(MA1, MA2) and rsi(close,14) > 50
+    lun1 = label.new(bar_index, na, "Go Short\n"+tostring(truncate(MA1,2))+breakLines, 
       color=color.new(color.red, transparency), 
-      textcolor=color.white,
-      style=label.style_labeldown, size=size.tiny)
+      textcolor=color.red,
+      style=label.style_labeldown, size=size.normal)
     label.set_y(lun1, MA1)
-if ShowCrosses and MA1Visible and MA2Visible and crossover(MA1, MA2)
-    lup1 = label.new(bar_index, na,  breakLines+"Long",
+if ShowCrosses and MA1Visible and MA2Visible and crossover(MA1, MA2) and rsi(close,14) < 60
+    lup1 = label.new(bar_index, bar_index*2,  breakLines+"Go Long\n"+tostring(truncate(MA1,2)),
       color=color.new(color.green, transparency), 
-      textcolor=color.white,
-      style=label.style_labelup, size=size.tiny)
+      textcolor=color.green,
+      style=label.style_labelup, size=size.normal)
     label.set_y(lup1, MA1)
 if ShowCrosses and MA1Visible and MA3Visible and crossunder(MA1, MA3)
     lun2 = label.new(bar_index, na,  "Short"+breakLines,
