5b0f126d60951cc934161063dc2eb8c97405e8c9
Alexander Kentros <crAlexcomp@gmail.com>
2021-03-19 11:42:26 +0200

    Using the new Pine Editor group command

--- GoldenIndicator.js
+++ GoldenIndicator.js
@@ -77,35 +77,30 @@ ma(type,source,period) =>
 
 //-------------------- INPUTS -----------------------------------------------
 
-// AIHelp = input(title="AI HELP EXPERIMENTAL*", type=input.bool, defval=true) 
-AIHelp = false
-ShowRibbons = input(title="Show Ribbons", type=input.bool, defval=true)
+MA1Visible = input(title="Visible", type=input.bool, defval=true , group = "Moving Average 1")
+MA1Period = input(6, title="Period", group = "Moving Average 1")
+MA1Type = input(title="Type", defval="EMA - Exponential Moving Average", options=["SMA - Simple Moving Average", "EMA - Exponential Moving Average","RMA - Rolling Moving Average",  "WMA - Weighted Moving Average", "HMA - Hull Moving Average", "DMA - Double Exponential Moving Average", "TMA - Triple Exponential Moving Average"], group = "Moving Average 1")
+MA1Source = input(title="Source", type=input.source, defval=close, group = "Moving Average 1")
+MA1TimeFrame = input(title="Timeframe", defval="Current", options=["Current", "1m", "3m", "5m", "15m", "30m", "45m", "1h", "2h", "3h", "4h", "1D", "1W", "1M"], group = "Moving Average 1")
+
+MA2Visible = input(title="Visible", type=input.bool, defval=true, group = "Moving Average 2")
+MA2Period = input(21, title="Moving Average 2 Period", group = "Moving Average 2")
+MA2Type = input(title="Type", defval="EMA - Exponential Moving Average", options=["SMA - Simple Moving Average", "EMA - Exponential Moving Average","RMA - Rolling Moving Average",  "WMA - Weighted Moving Average", "HMA - Hull Moving Average", "DMA - Double Exponential Moving Average", "TMA - Triple Exponential Moving Average"], group = "Moving Average 2")
+MA2Source = input(title="Source", type=input.source, defval=close, group = "Moving Average 2")
+MA2TimeFrame = input(title="Timeframe", defval="Current", options=["Current", "1m", "3m", "5m", "15m", "30m", "45m", "1h", "2h", "3h", "4h", "1D", "1W", "1M"], group = "Moving Average 2")
 
 
-MA1Visible = input(title="------------ Moving Average 1 Visible ------------", type=input.bool, defval=true)
-MA1Period = input(6, title="Moving Average 1 Period")
-MA1Type = input(title="Moving Average 1 Type", defval="EMA - Exponential Moving Average", options=["SMA - Simple Moving Average", "EMA - Exponential Moving Average","RMA - Rolling Moving Average",  "WMA - Weighted Moving Average", "HMA - Hull Moving Average", "DMA - Double Exponential Moving Average", "TMA - Triple Exponential Moving Average"])
-MA1Source = input(title="Moving Average 1 Source", type=input.source, defval=close)
-MA1TimeFrame = input(title="Moving Average 1 Timeframe", defval="Current", options=["Current", "1m", "3m", "5m", "15m", "30m", "45m", "1h", "2h", "3h", "4h", "1D", "1W", "1M"])
+MA3Visible = input(title="Visible", type=input.bool, defval=true, group = "Moving Average 3")
+MA3Period = input(50, title="Period", group = "Moving Average 3")
+MA3Type = input(title="Type", defval="EMA - Exponential Moving Average", options=["SMA - Simple Moving Average", "EMA - Exponential Moving Average","RMA - Rolling Moving Average",  "WMA - Weighted Moving Average", "HMA - Hull Moving Average", "DMA - Double Exponential Moving Average", "TMA - Triple Exponential Moving Average"], group = "Moving Average 3")
+MA3Source = input(title="Source", type=input.source, defval=close, group = "Moving Average 3")
+MA3TimeFrame = input(title="Timeframe", defval="Current", options=["Current", "1m", "3m", "5m", "15m", "30m", "45m", "1h", "2h", "3h", "4h", "1D", "1W", "1M"], group = "Moving Average 3")
 
-MA2Visible = input(title="------------ Moving Average 2 Visible ------------", type=input.bool, defval=true)
-MA2Period = input(21, title="Moving Average 2 Period")
-MA2Type = input(title="Moving Average 2 Type", defval="EMA - Exponential Moving Average", options=["SMA - Simple Moving Average", "EMA - Exponential Moving Average","RMA - Rolling Moving Average",  "WMA - Weighted Moving Average", "HMA - Hull Moving Average", "DMA - Double Exponential Moving Average", "TMA - Triple Exponential Moving Average"])
-MA2Source = input(title="Moving Average 2 Source", type=input.source, defval=close)
-MA2TimeFrame = input(title="Moving Average 2 Timefram", defval="Current", options=["Current", "1m", "3m", "5m", "15m", "30m", "45m", "1h", "2h", "3h", "4h", "1D", "1W", "1M"])
-
-
-MA3Visible = input(title="------------ Moving Average 3 Visible ------------", type=input.bool, defval=true)
-MA3Period = input(50, title="Moving Average 3 Period")
-MA3Type = input(title="Moving Average 3 Type", defval="EMA - Exponential Moving Average", options=["SMA - Simple Moving Average", "EMA - Exponential Moving Average","RMA - Rolling Moving Average",  "WMA - Weighted Moving Average", "HMA - Hull Moving Average", "DMA - Double Exponential Moving Average", "TMA - Triple Exponential Moving Average"])
-MA3Source = input(title="Moving Average 3 Source", type=input.source, defval=close)
-MA3TimeFrame = input(title="Moving Average 3 Timeframe", defval="Current", options=["Current", "1m", "3m", "5m", "15m", "30m", "45m", "1h", "2h", "3h", "4h", "1D", "1W", "1M"])
-
-MA4Visible = input(title="------------ Moving Average 4 Visible ------------", type=input.bool, defval=true)
-MA4Period = input(150, title="Moving Average 4 Period")
-MA4Type = input(title="Moving Average 4 Type", defval="EMA - Exponential Moving Average", options=["SMA - Simple Moving Average", "EMA - Exponential Moving Average","RMA - Rolling Moving Average",  "WMA - Weighted Moving Average", "HMA - Hull Moving Average", "DMA - Double Exponential Moving Average", "TMA - Triple Exponential Moving Average"])
-MA4Source = input(title="Moving Average 4 Source", type=input.source, defval=close)
-MA4TimeFrame = input(title="Moving Average 4 Timeframe", defval="Current", options=["Current", "1m", "3m", "5m", "15m", "30m", "45m", "1h", "2h", "3h", "4h", "1D", "1W", "1M"])
+MA4Visible = input(title="Visible", type=input.bool, defval=false, group = "Moving Average 4")
+MA4Period = input(150, title="Period", group = "Moving Average 4")
+MA4Type = input(title="Type", defval="EMA - Exponential Moving Average", options=["SMA - Simple Moving Average", "EMA - Exponential Moving Average","RMA - Rolling Moving Average",  "WMA - Weighted Moving Average", "HMA - Hull Moving Average", "DMA - Double Exponential Moving Average", "TMA - Triple Exponential Moving Average"], group = "Moving Average 4")
+MA4Source = input(title="Source", type=input.source, defval=close, group = "Moving Average 4")
+MA4TimeFrame = input(title="Timeframe", defval="Current", options=["Current", "1m", "3m", "5m", "15m", "30m", "45m", "1h", "2h", "3h", "4h", "1D", "1W", "1M"], group = "Moving Average 4")
 
 
 MA1 = security(syminfo.tickerid, resolution(MA1TimeFrame), ma(MA1Type, MA1Source, MA1Period))     
@@ -121,44 +116,18 @@ plot(MA2Visible ? MA2 : na,color = color.new(color.red, maColorTransparency),tit
 plot(MA3Visible ? MA3 : na,color = color.new(color.blue, maColorTransparency),title="Moving Average 3 Color", linewidth = 3)
 plot(MA4Visible ? MA4 : na,color = color.new(color.purple, maColorTransparency),title="Moving Average 4 Color", linewidth = 3)
 
-
-//----------------------------- AI HELPER LONG AND SHORT POSITIONS -------------------------------------------------
-
-long  = MA1 > MA2
-short = MA1 < MA2
-
-longCondition = not long[5]
-shortCondition = not short[5]
-
-// closeLong = MA1 < MA2 and not long[11]
-// closeShort = MA1 > MA2 and not short[11]
-
-isSuperPenny = high < 2
-biggerThan20000 = high > 20000
-longPositionVerticalOffset = (low*(isSuperPenny ? 1 : biggerThan20000 ? 18 : 8))/100 
-shortPositionVerticalOffset =  (high*(isSuperPenny ? 1 : biggerThan20000 ? 15 : 5))/100
-
-breakLines= ""
-colorTransparency = 0
-
-if(AIHelp)
-    if rsi(close,14) >= 60 and short and not short[11] //and MA1 - MA2 < 6  and shortCondition
-        lun1 = label.new(bar_index,high + shortPositionVerticalOffset,tostring(truncate(MA1,isSuperPenny?5:2))+breakLines,color=color.new(color.red, colorTransparency), textcolor=color.red, size=size.normal , style=label.style_arrowdown)
-    if rsi(close,14) <= 30 and long //and MA2 - MA1 < 10 and longCondition
-        lup1 = label.new(bar_index,low - longPositionVerticalOffset,  breakLines+""+tostring(truncate(MA1,isSuperPenny?5:2)),color=color.new(color.green, colorTransparency), textcolor=color.green, size=size.normal , style=label.style_arrowup)
-        
 // --------------------------  FORECASTING ------------------------------------------
-ShowForecasts = input(title="------------ Show Forecasts ------------", type=input.bool, defval=true)
+ShowForecasts = input(title="Visible", type=input.bool, defval=true, group = "Forecast")
 
 // RsiInfuenceBias = input(title="Calculate Bias based on RSI", type=input.bool, defval=true)
 a = "Neutral"
 // if RsiInfuenceBias == true
 //     a = "Bullish"
-ForecastBias = input(title="Forecast Bias", defval=a, options=["Neutral", "Bullish", "Bearish"])
+ForecastBias = input(title="Bias", defval=a, options=["Neutral", "Bullish", "Bearish"], group = "Forecast")
 
 
-ForecastBiasPeriod = input(14, title="Forecast Bias Period")
-ForecastBiasMagnitude = input(1, title="Forecast Bias Magnitude", minval=0.25, maxval=20, step=0.25)
+ForecastBiasPeriod = input(14, title="Bias Period", group = "Forecast")
+ForecastBiasMagnitude = input(1, title="Bias Magnitude", minval=0.25, maxval=20, step=0.25, group = "Forecast")
 
 // Forecasting - forcasted prices are calculated using our MAType and MASource for the MAPeriod - the last X candles.
 //              it essentially replaces the oldest X candles, with the selected source * X candles
@@ -231,6 +200,7 @@ plot(MA4TimeFrame == "Current" and ShowForecasts and MA4Visible ? MA4Forecast1 :
 // label.new(bar_index, high + 15, tostring(5))
 
 // --------------------------  RIBBONS ------------------------------------------
+ShowRibbons = input(title="Show Ribbons", type=input.bool, defval=false , group = "Advanced")
 
 // Ribbon related code
 // For Ribbons to work - they must use the same MAType, MAResolution and MASource.  This is to ensure the ribbons are fair between one to the other.
@@ -281,3 +251,32 @@ plot(MA3Visible and MA4Visible and ShowRibbons and MA3Type == MA4Type and MA3Tim
 plot(MA3Visible and MA4Visible and ShowRibbons and MA3Type == MA4Type and MA3TimeFrame == MA4TimeFrame and MA3Source == MA4Source ? Ribbon14 : na, color=color.purple, linewidth=1, style=plot.style_line, title="Ribbon14", transp=85)
 plot(MA3Visible and MA4Visible and ShowRibbons and MA3Type == MA4Type and MA3TimeFrame == MA4TimeFrame and MA3Source == MA4Source ? Ribbon15 : na, color=color.purple, linewidth=1, style=plot.style_line, title="Ribbon15", transp=90)
 
+
+
+//----------------------------- AI HELPER LONG AND SHORT POSITIONS -------------------------------------------------
+
+AIHelp = input(title="AI HELP EXPERIMENTAL*", type=input.bool, defval=false , group = "Advanced") 
+
+long  = MA1 > MA2
+short = MA1 < MA2
+
+longCondition = not long[5]
+shortCondition = not short[5]
+
+// closeLong = MA1 < MA2 and not long[11]
+// closeShort = MA1 > MA2 and not short[11]
+
+isSuperPenny = high < 2
+biggerThan20000 = high > 20000
+longPositionVerticalOffset = (low*(isSuperPenny ? 1 : biggerThan20000 ? 18 : 8))/100 
+shortPositionVerticalOffset =  (high*(isSuperPenny ? 1 : biggerThan20000 ? 15 : 5))/100
+
+breakLines= ""
+colorTransparency = 0
+
+if(AIHelp)
+    if rsi(close,14) >= 60 and short //and not short[11] //and MA1 - MA2 < 6  and shortCondition
+        lun1 = label.new(bar_index,high + shortPositionVerticalOffset,tostring(truncate(MA1,isSuperPenny?5:2))+breakLines,color=color.new(color.red, colorTransparency), textcolor=color.red, size=size.normal , style=label.style_arrowdown)
+    if rsi(close,14) <= 30 and long //and MA2 - MA1 < 10 and longCondition
+        lup1 = label.new(bar_index,low - longPositionVerticalOffset,  breakLines+""+tostring(truncate(MA1,isSuperPenny?5:2)),color=color.new(color.green, colorTransparency), textcolor=color.green, size=size.normal , style=label.style_arrowup)
+        
