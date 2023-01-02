6171783ced36d7467f72edf7665125f6130ffe11
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2023-01-02 12:11:58 +0200

    Update to Pinescript 5
    https://www.tradingview.com/pine-script-docs/en/v5/migration_guides/To_Pine_version_5.html

--- GoldenRSI.js
+++ GoldenRSI.js
@@ -1,15 +1,16 @@
-//@version=4
-study(title="Relative Strength Index", shorttitle="Golden RSI", format=format.price, precision=2, resolution="")
+//@version=5
+indicator(title='Relative Strength Index', shorttitle='Golden RSI', format=format.price, precision=2, timeframe='')
 
 
-rsiLength = input(14, minval=1, title="Length")
-rsiSource = input(close, "Source", type = input.source)
-rsi = rsi(rsiSource, rsiLength)
+rsiLength = input.int(14, minval=1, title='Length')
+rsiSource = input(close, 'Source')
+rsi = ta.rsi(rsiSource, rsiLength)
 
-plot(rsi, "RSI", color=color.yellow)
+plot(rsi, 'RSI', color=color.new(color.yellow, 0))
 
 
-band1 = hline(70, "Upper Band", color=#C0C0C0)
-band0 = hline(30, "Lower Band", color=#C0C0C0)
+band1 = hline(70, 'Upper Band', color=#C0C0C0)
+band0 = hline(30, 'Lower Band', color=#C0C0C0)
 
-fill(band1, band0, color=color.new(color.yellow,95), transp=90, title="Background")
+fill(band1, band0, color=color.new(color.yellow, 95), title='Background', transp=90)
+
